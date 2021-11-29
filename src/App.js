0edc763690ebde4f244affa5948976b3d0a0280e
Benllshua <benshua2@gmail.com>
2021-11-29 12:45:47 +0200

    its your time now, do or dont! there is no try

--- src/App.js
+++ src/App.js
@@ -1,44 +1,13 @@
-import "./App.css";
-import { useRef, useState } from "react";
-import { Pose } from "@mediapipe/pose";
-
-import Canvas from "./components/Canvas";
-import Livestream from "./components/Livestream";
+import React from "react";
+import CamView from "./components/CamView/CamView";
 
 function App() {
-
-  const [results, setResults] = useState(undefined)
-  
-  const canvasRef = useRef(null);
-  const webCamRef = useRef(null);
-
-  const myPose = new Pose({
-    locateFile: (file) => {
-      return `https://cdn.jsdelivr.net/npm/@mediapipe/pose/${file}`;
-    },
-  });
-
-  //pose settings
-  myPose.setOptions({
-    modelComplexity: 1,
-    smoothLandmarks: true,
-    enableSegmentation: true,
-    smoothSegmentation: true,
-    minDetectionConfidence: 0.5,
-    minTrackingConfidence: 0.5,
-  });
-
-  //wating for function and gets parameter
-  myPose.onResults((results) => setResults(results));
-
-  return (
-    <div>
-      <h1>my app</h1>
-      <Livestream webCamRef={webCamRef} myPose={myPose}/>
-      <Canvas canvasRef={canvasRef} results={results}/>
-      
-    </div>
-  );
+	return (
+		<div>
+			<h1>my app</h1>
+			<CamView />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/CamView/CamView.js
@@ -0,0 +1,48 @@
+import { useRef, useState } from "react";
+
+// components
+import { Paper, Switch } from "@mui/material";
+import Canvas from "./Canvas";
+import Livestream from "./Livestream";
+
+// ready to use API (https://google.github.io/mediapipe/getting_started/javascript.html#ready-to-use-javascript-solutions)
+import { Pose } from "@mediapipe/pose";
+
+function CamView() {
+	const [results, setResults] = useState(undefined);
+	const [showMesh, setShowMesh] = useState(false);
+
+	const canvasRef = useRef(null);
+	const webCamRef = useRef(null);
+
+	const myPose = new Pose({
+		locateFile: (file) => {
+			return `https://cdn.jsdelivr.net/npm/@mediapipe/pose/${file}`;
+		},
+	});
+
+	//pose settings
+	myPose.setOptions({
+		modelComplexity: 1,
+		smoothLandmarks: true,
+		enableSegmentation: true,
+		smoothSegmentation: true,
+		minDetectionConfidence: 0.5,
+		minTrackingConfidence: 0.5,
+	});
+
+	//wating for function and gets parameter
+	myPose.onResults((results) => setResults(results));
+
+	return (
+		<Paper>
+			<Switch color={"primary"} value={showMesh} onClick={() => setShowMesh(!showMesh)} />
+			<div>
+				<Livestream webCamRef={webCamRef} myPose={myPose} />
+				{showMesh && <Canvas canvasRef={canvasRef} results={results} />}
+			</div>
+		</Paper>
+	);
+}
+
+export default CamView;
